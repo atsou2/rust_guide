049e4a322ce03009b372ee531ec78d9009743651
atsou2 <atsou2@jh.edu>
2024-09-26 19:53:42 -0400

    copied kaze code

--- src/sampler.rs
+++ src/sampler.rs
@@ -1,27 +1,50 @@
 use rand::{prelude::Distribution, rngs::StdRng, Rng, SeedableRng};
 use statrs::distribution::MultivariateNormal;
+
 pub struct State<const N_DIM: usize> {
-    // Fill this
+    rng: StdRng,
+    pub arr: [f64; N_DIM],
+    proposal_distribution: MultivariateNormal,
 }
 
 fn log_likelihood<const N_DIM: usize>(arr: &[f64]) -> f64 {
-    // Define the mean vector and covariance matrix
-    let mean = vec![0.0; N_DIM]; // Assuming mean is a zero vector
-    let cov = vec![vec![1.0; N_DIM]; N_DIM]; // Assuming identity matrix for covariance
-
-    // Create a MultivariateNormal distribution
-    let mvn = MultivariateNormal::new(mean, cov).unwrap();
-
-    // Compute the log likelihood
-    mvn.logpdf(arr)
+    let mut sum = 0.0;
+    for i in 0..N_DIM {
+        sum += -(arr[i] * arr[i] / 2.0) as f64;
+    }
+    sum
 }
 
 impl<const N_DIM: usize> State<N_DIM> {
     pub fn new(seed: u64) -> Self {
-    // Fill this
+        let rng = StdRng::seed_from_u64(seed);
+        let arr = [0.0; N_DIM];
+        let mean = vec![0.0; N_DIM];
+        let mut cov = Vec::<f64>::new();
+        for i in 0..N_DIM {
+            for j in 0..N_DIM {
+                if i == j {
+                    cov.push(1.0);
+                } else {
+                    cov.push(0.0);
+                }
+            }
+        }
+        let proposal_distribution = MultivariateNormal::new(mean, cov).unwrap();
+        Self { rng, arr, proposal_distribution }
     }
 
     pub fn take_step(&mut self) {
-    // Fill this
+        let binding = self.proposal_distribution.sample(&mut self.rng);
+        let proposal= binding.as_slice();
+        let log_alpha = log_likelihood::<N_DIM>(proposal) - log_likelihood::<N_DIM>(&self.arr);
+
+        if log_alpha > (self.rng.gen_range(0.0..1.0) as f64).ln() {
+            let mut new_location: [f64; N_DIM] = [0.0; N_DIM];
+            for i in 0..N_DIM {
+                new_location[i] = proposal[i];
+            }
+            self.arr = new_location;
+        }
     }
 }
